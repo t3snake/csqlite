47f4822a3ddff34e930d4902ecda4a6f4a1d4e7b
t3snake <t3snake@gmail.com>
2026-03-15 15:11:44 +0530

    print no of tables in .dbinfo by reading b-tree page header

--- src/main.c
+++ src/main.c
@@ -18,16 +18,30 @@ int main(int argc, char *argv[]) {
             return 1;
         }
 
-        fseek(database_file, 16, SEEK_SET);  // Skip the first 16 bytes of the header
+        fprintf(stderr, "Logs from your program will appear here!\n");
+
+        // Skip the first 16 bytes of the header
+        fseek(database_file, 16, SEEK_SET);
+
+        // Read next 2 bytes to get page size
         unsigned char buffer[2];
         fread(buffer, 1, 2, database_file);
         unsigned short page_size = (buffer[1] | (buffer[0] << 8)); // big endian
 
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        fprintf(stderr, "Logs from your program will appear here!\n");
-
-        // TODO: Uncomment the code below to pass the first stage
         printf("database page size: %u\n", page_size);
+
+        // Skip next 84 bytes ie. DB Header (100 total along with already skipped 16 bytes)
+        fseek(database_file, 84, SEEK_CUR);
+
+        // Next 8 to 12 bytes are b-tree page headers
+        // At offset 3, the 2 byte integer gives number of cells on the page
+        // Since this b-tree is the internal sqlite_schema table, the number of rows is the number of tables (if only tables in DB)
+        fseek(database_file, 3, SEEK_CUR);
+        fread(buffer, 1, 2, database_file);
+
+        unsigned short cell_count = (buffer[1] | (buffer[0] << 8));
+
+        printf("number of tables: %u\n", cell_count);
 
         fclose(database_file);
     } else {
